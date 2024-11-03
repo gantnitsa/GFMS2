b221be3f7c199498ebd3655b043c9386dc930ed9
gantnitsa <111276875+gantnitsa@users.noreply.github.com>
2024-11-03 14:28:09 +0000

    Update HOME.py

--- GFMS2/resources/HOME.py
+++ GFMS2/resources/HOME.py
@@ -4,4 +4,4 @@ class HOME:
         if IN == '':
             return([], '\'HOME\' initialised')
         else:
-            return([], '\033[38;2;255;;m\'HOME\' doesn\'t do anything. Please do not give it a command')
+            return([], '\033[38;2;255;;m\'HOME\' doesn\'t do anything. Please do not give it a command\033[0m')
