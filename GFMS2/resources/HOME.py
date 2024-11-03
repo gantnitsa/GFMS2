8b22cd73c706363c7b63dd58c383b095980656f9
gantnitsa <111276875+gantnitsa@users.noreply.github.com>
2024-11-03 15:00:22 +0000

    Delete GFMS2 directory

--- GFMS2/resources/HOME.py
+++
@@ -1,7 +0,0 @@
-class HOME:
-    def __init__(self): pass
-    def CMD(self, IN:str, DTSYS:tuple):
-        if IN == '':
-            return([[], '\'HOME\' initialised'])
-        else:
-            return([[], '\033[38;2;255;;m\'HOME\' doesn\'t do anything. Please do not give it a command\033[0m'])
--- GFMS2/resources/LOGGER.py
+++
@@ -1,4 +0,0 @@
-class LOGGER:
-    def __init__(self): pass
-    def CMD(self, IN:str, DTSYS:tuple):
-        return([], '')
