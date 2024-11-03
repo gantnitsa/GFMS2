777b1a3e206df4667a102105e7201cc8d848f452
gantnitsa <111276875+gantnitsa@users.noreply.github.com>
2024-11-03 14:38:34 +0000

    Add files via upload

--- GFMS2/main.py
+++ GFMS2/main.py
@@ -31,14 +31,10 @@ def assemble():
         POUT = POUT[0:-1]
     del POUT
     PTABS = ([f'TAB PAGE: {SUB_PAGE}{' '*39}'[0:39] + '\033[38;2;128;;128m█\033[0m', f'\033[38;2;128;;128m{'▄'*39}\033[0m\033[38;2;128;;128m█\033[0m'] + [(str(i + 1) + '. ' + SUB_LIST[i][0] + ' '*39)[0:39] + '\033[38;2;128;;128m█\033[0m' for i in range(len(SUB_LIST))][slice((SUB_PAGE - 1)*30, SUB_PAGE*30)] + [f'{' '*39}\033[38;2;128;;128m█\033[0m' for i in range(32)])[0:32]
-    return([PTABS[i] + PPOUT[i] for i in range(32)] + [f'\033[38;2;128;;128m{'▄'*39}█{'▄'*100}\033[0m', f'\033[38;2;;;255mPREV_CMD: {CMD}\033[0m', OUT[1]])
+    return([PTABS[i] + PPOUT[i] for i in range(32)] + [f'\033[38;2;128;;128m{'▄'*39}█{'▄'*100}\033[0m', f'\033[38;2;;;200mPREV_CMD: {CMD}\033[0m', OUT[1]])
 
-#basic mustknows
-def TOP(): return("   ____   _____   __  __   ____    ____  ", "  / ___| |  ___| |  \\/  | / ___|  |___ \\ " + " "*87 + "\x1b[38;2;;255;mLAST REFRESH\x1b[0m", " | |  _  | |_    | |\\/| | \\___ \\    __) |" + " "*88 + f'\x1b[38;2;;200;m{str(datetime.datetime.now())[8:10] + ' ' + ('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')[int(str(datetime.datetime.now())[5:7]) - 1] + ' ' + str(datetime.datetime.now())[0:4]}\x1b[0m', " | |_| | |  _|   | |  | |  ___) |  / __/ " + " "*94 + f'\x1b[38;2;;200;m{str(datetime.datetime.now())[11:16]}\x1b[0m', "  \\____| |_|     |_|  |_| |____/  |_____|", f'\033[38;2;128;;128m{'▄'*140}\033[0m')
-
+#Sys info
 def DTSYS(): return(int(''.join(str(datetime.datetime.now())[0:10].split('-'))), int(''.join(str(datetime.datetime.now())[11:16].split(':')))) #System read [0]: date(YYYYMMDD)(len = 8), [1]: time(HHMM)(len = 4)
-
-#int(''.join(str(os.get_terminal_size())[25:len(str(os.get_terminal_size()))][i] for i in range(len(str(os.get_terminal_size())[25:len(str(os.get_terminal_size()))])) if not ',' in str(os.get_terminal_size())[25:len(str(os.get_terminal_size()))][0:(i + 1)]))
 
 #vars
 CMD = '' #input controller
@@ -49,12 +45,12 @@ OUT = [[], ''] #plugin output
 
 #main
 while CMD != 'TERMINATE':
-    print('\x1b[2J\x1b[H', end = '')
+    os.system('cls')
     if int(''.join(str(os.get_terminal_size())[25:len(str(os.get_terminal_size()))][i] for i in range(len(str(os.get_terminal_size())[25:len(str(os.get_terminal_size()))])) if not ',' in str(os.get_terminal_size())[25:len(str(os.get_terminal_size()))][0:(i + 1)])) < 140 and CUR_SUB != 'WIDTH_ERROR':
         print('\x1b[38;2;255;;mPLEASE RESIZE SCREEN WIDTH TO 140 CHARS\x1b[0m')
         CUR_SUB = 'WIDTH_ERROR'
     else:
-        for i in TOP(): print(i)
+        for i in ("   ____   _____   __  __   ____    ____  ", "  / ___| |  ___| |  \\/  | / ___|  |___ \\ " + " "*87 + "\x1b[38;2;;255;mLAST REFRESH\x1b[0m", " | |  _  | |_    | |\\/| | \\___ \\    __) |" + " "*88 + f'\x1b[38;2;;200;m{str(datetime.datetime.now())[8:10] + ' ' + ('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')[int(str(datetime.datetime.now())[5:7]) - 1] + ' ' + str(datetime.datetime.now())[0:4]}\x1b[0m', " | |_| | |  _|   | |  | |  ___) |  / __/ " + " "*94 + f'\x1b[38;2;;200;m{str(datetime.datetime.now())[11:16]}\x1b[0m', "  \\____| |_|     |_|  |_| |____/  |_____|", f'\033[38;2;128;;128m{'▄'*140}\033[0m'): print(i)
         if CUR_SUB in ('WIDTH_ERROR', 'INIT'):
             CUR_SUB = 'HOME'
             OUT = [[], '']
--- GFMS2/resources/HOME.py
+++ GFMS2/resources/HOME.py
@@ -2,6 +2,6 @@ class HOME:
     def __init__(self): pass
     def CMD(self, IN:str, DTSYS:tuple):
         if IN == '':
-            return([], '\'HOME\' initialised')
+            return([[], '\'HOME\' initialised'])
         else:
-            return([], '\033[38;2;255;;m\'HOME\' doesn\'t do anything. Please do not give it a command\033[0m')
+            return([[], '\033[38;2;255;;m\'HOME\' doesn\'t do anything. Please do not give it a command\033[0m'])
