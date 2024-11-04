0c5e57c8574fa714f67648fde71dbe63ac80e77a
gantnitsa <astintang923@gmail.com>
2024-11-04 16:47:32 +0000

    Home is no longer angry at you

--- B-GFMS2-v0-0-1/resources/home/home.py
+++ B-GFMS2-v0-0-1/resources/home/home.py
@@ -3,10 +3,7 @@ import os
 class home:
     def __init__(self): pass
     def CMD(self, IN:str, DTSYS:tuple):
-        if IN in ('', 'Init'):
-            return([['Home sweet home', '', 'Fact of the second:', facto(), '', 'Here\'s a house', '° , ° ˚___Π_____*☽*˚ ,', '✩ ˚˛˚*/______/__\\。✩˚ ˚', '˚ ˛˚˛｜ 田田｜門｜ ˚ ˚', '\' ̛ ̛ \'\' \'\' \'\' ̛ ̛ \'\' \'\' \'\'', '', 'Command Help:', commands(0) + ': ends the program', commands(1) + ': scrolls to specified page', commands(2) + ': switches tabs to specified program'], '\'home\' refreshed. Enjoy your new fact!'])
-        else:
-            return([[f'BRO,', '\033[38;2;255;;m  _   _    ___  \033[0m', '\033[38;2;255;;m | \\ | |  / _ \\ \033[0m', '\033[38;2;255;;m |  \\| | | | | |\033[0m', '\033[38;2;255;;m | |\\  | | |_| |\033[0m', '\033[38;2;255;;m |_| \\_|  \\___/ \033[0m', '', 'Press \'enter\' to continue out of your mistake.', '', 'Fact of the second:\nYou should not have done that.'], '\033[38;2;255;;m\'home\' doesn\'t do anything. Please do not give it a command\033[0m'])
+        return([['Home sweet home', '', 'Fact of the second:', facto(), '', 'Here\'s a house', '° , ° ˚___Π_____*☽*˚ ,', '✩ ˚˛˚*/______/__\\。✩˚ ˚', '˚ ˛˚˛｜ 田田｜門｜ ˚ ˚', '\' ̛ ̛ \'\' \'\' \'\' ̛ ̛ \'\' \'\' \'\'', '', 'Command Help:', commands(0) + ': ends the program', commands(1) + ': scrolls to specified page', commands(2) + ': switches tabs to specified program'], '\'home\' refreshed. Enjoy your new fact!'])
 def facto():
     with open('home\\Facts.txt') as FH:
         i = FH.read().split('\n')
